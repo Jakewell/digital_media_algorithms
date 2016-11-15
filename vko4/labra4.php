28bef169ce918267bc624c6d0185d3acc03f6ca8
Jakewell <jarkko.saikkonen@gmail.com>
2016-11-15 17:40:25 +0200

    vko4update

--- vko4/labra4.php
+++ vko4/labra4.php
@@ -3,14 +3,15 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>Labra 3 - Jarkko Saikkonen</title>
+	<title>Labra 4 - Jarkko Saikkonen</title>
 	<script type="text/javascript" src="../jquery-3.1.1.min.js"></script>
 	<script type="text/javascript" src="labra4.js"></script>
 	<link rel="stylesheet" type="text/css" href="labra4.css">
 </head>
 
 <body>
-	<h2>Click on a table cell to select it</h2>
+    <p>Stack: Voidaan esimerkiksi PHP-kielessä nähdä virheen sattuessa "stack trace", eli pystytään löytämään virhekohta helpommin.</p>
+    <p>Queue: Seuraavaksi suoritettavissa komennoissa tiedetään varmasti, että ensimmäisenä suoritettava komento todella suoritetaan ensimmäisenä.</p>
 	<table id="infoTable">
 	</table>
 	
