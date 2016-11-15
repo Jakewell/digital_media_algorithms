8c696670b8025320ba514871958d0a44f7876aaa
Jakewell <jarkko.saikkonen@gmail.com>
2016-11-15 17:23:32 +0200

    vko4

---
+++ vko4/labra4.js
@@ -0,0 +1,49 @@
+$(function () {
+	var infoArray = [
+		{
+			firstname: "Juho",
+			lastname: "Rantanen",
+			work: "Tutkimus eri paperilajien hiilijalanjäljistä"
+		},
+		
+		{
+			firstname: "Lauri",
+			lastname: "Hynninen",
+			work: "Videopilvipalvelut markkinointimateriaalin julkaisukanavana"
+		},
+		
+		{
+			firstname: "Satu",
+			lastname: "Pohjola",
+			work: "Biometrisen datan hallinta ja visualisointi selaimessa"
+		},
+		
+		{
+			firstname: "Jaakko",
+			lastname: "Tervonen",
+			work: "Markkinointivideon suunnittelu, käsikirjoittaminen, ohjaaminen ja leikkaaminen"
+		}
+	];
+	
+	for (var i = 0; i < infoArray.length; i++) {
+		$("#infoTable").append("<tr>" +
+								  "<td>" + infoArray[i].firstname + "</td>" +
+								  "<td>" + infoArray[i].lastname + "</td>" +
+								  "<td>" + infoArray[i].work + "</td>" +
+							   "</tr>");
+	}
+
+	$("#stackAndQueueButton").on("click", function(e) {
+		e.preventDefault();
+		
+		$.ajax({
+			url: "stackqueue.php",
+			type: 'POST',
+			data: {array: infoArray},
+			success: function(result) {
+				$("#resultField").empty();
+				$("#resultField").append(result);
+			}
+		});
+	});
+});
---
+++ vko4/labra4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Labra 3 - Jarkko Saikkonen</title>
+	<script type="text/javascript" src="../jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="labra4.js"></script>
+	<link rel="stylesheet" type="text/css" href="labra4.css">
+</head>
+
+<body>
+	<h2>Click on a table cell to select it</h2>
+	<table id="infoTable">
+	</table>
+	
+	<div id="inputField">
+		<button id="stackAndQueueButton" type="button">Stack and Queue</button>
+	</div>
+	
+	<div id="resultField">
+	</div>
+</body>
+</html>
